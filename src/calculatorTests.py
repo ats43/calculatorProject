2ff8906908e56b4d43bc957ccf7bf4315a425eb1
ats43 <85419691+ats43@users.noreply.github.com>
2021-07-01 20:56:27 -0400

    Feature: added setUp method

--- src/calculatorTests.py
+++ src/calculatorTests.py
@@ -4,23 +4,23 @@ from calculator import Calculator #take calculator.py (module) and import the Ca
 
 class MyTestCase(unittest.TestCase): #unit tests are tests we write to test code. (TDD) test driven development
 
+    def setUp(self) -> None:
+        self.calculator = Calculator() #instantiates calculator in each test.
+
     def test_instantiate_calculator(self):
-        calculator = Calculator() #instaniates the calculator
-        self.assertIsInstance(calculator,Calculator)    #tests if there is a calculator (instance, isItAClass?)
+        #calculator = Calculator() #instaniates the calculator
+        self.assertIsInstance(self.calculator,Calculator)    #tests if there is a calculator (instance, isItAClass?)
 
     def test_results_property_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.result, 0)
+        self.assertEqual(self.calculator.result, 0)
 
     def test_add_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.add(2,3),5)
-        self.assertEqual(calculator.result,5)
+        self.assertEqual(self.calculator.add(2,3),5)
+        self.assertEqual(self.calculator.result,5)
 
     def test_subtract_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.subtract(2,3),-1)
-        self.assertEqual(calculator.result,-1)
+        self.assertEqual(self.calculator.subtract(2,3),-1)
+        self.assertEqual(self.calculator.result,-1)
 
 if __name__ == '__main__':
     unittest.main()
