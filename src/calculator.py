cf13cf6c32ed1ac8aef4872674b0e40be8a043bb
ats43 <85419691+ats43@users.noreply.github.com>
2021-07-01 20:41:22 -0400

    Feature: added add method and subtraction method

--- src/calculator.py
+++ src/calculator.py
@@ -1,21 +1,17 @@
 def addition(a, b):
     return a + b
 
-
 def subtraction(a, b):
     return a - b
 
-
-class Calculator:
-    result = 0
+class Calculator: #defines the blueprints of the object
+    result = 0 #temporary placeholder for the "result property"
 
     def __init__(self):  # Object Oriented Programming "Things you can go to the store and buy it = class / object"
-        x = 2 + 2
-        self.result = x
-        pass  # if you can't then it's a static class
+        pass
 
     def add(self, a, b):
-        self.result = a + b
+        self.result = addition(a, b)
         return self.result
 
     def subtract(self, a, b):
--- src/calculatorTests.py
+++ src/calculatorTests.py
@@ -1,10 +1,26 @@
 import unittest
-from calculator import Calculator
+from calculator import Calculator #take calculator.py (module) and import the Calculator (class)
 
 
-class MyTestCase(unittest.TestCase):
+class MyTestCase(unittest.TestCase): #unit tests are tests we write to test code. (TDD) test driven development
+
     def test_instantiate_calculator(self):
+        calculator = Calculator() #instaniates the calculator
+        self.assertIsInstance(calculator,Calculator)    #tests if there is a calculator (instance, isItAClass?)
+
+    def test_results_property_calculator(self):
         calculator = Calculator()
-        self.assertIsInstance(calculator,Calculator)
+        self.assertEqual(calculator.result, 0)
+
+    def test_add_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.add(2,3),5)
+        self.assertEqual(calculator.result,5)
+
+    def test_subtract_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.subtract(2,3),-1)
+        self.assertEqual(calculator.result,-1)
+
 if __name__ == '__main__':
     unittest.main()
