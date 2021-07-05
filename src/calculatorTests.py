bec0fa0b9b7a6596b245169bb1c8e86611369ede
ats43 <85419691+ats43@users.noreply.github.com>
2021-07-05 11:41:39 -0400

    feature: all methods are working with all csv files

--- src/calculatorTests.py
+++ src/calculatorTests.py
@@ -28,38 +28,39 @@ class MyTestCase(unittest.TestCase):    # unit tests are tests we write to test 
             self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
         print("Completed Subtraction Test")
 
-    # def test_multiplication_method_calculator(self):
-    #     test_data = CsvReader('/src/Unit Test Multiplication.csv').data
-    #     pprint(test_data)
-    #     for row in test_data:
-    #         self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
-    #         self.assertEqual(self.calculator.result, int(row['Result']))
-    #
-    # def test_division_method_calculator(self):
-    #     test_data = CsvReader('/src/Unit Test Division.csv').data
-    #     pprint(test_data)
-    #     for row in test_data:
-    #         self.assertAlmostEqual(self.calculator.divide(row['Value 1'], row['Value 2']), float(row['Result']))
-    #         self.assertAlmostEqual(self.calculator.result, float(row['Result']))
+    def test_multiplication_method_calculator(self):
+        print("Begin Multiplication Test")
+        test_data = CsvReader('/src/Unit Test Multiplication.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
+        print("Completed Multiplication Test")
+
+    def test_division_method_calculator(self):
+        print("Begin Division Test")
+        test_data = CsvReader('/src/Unit Test Division.csv').data
+        for row in test_data:
+            self.assertAlmostEqual(self.calculator.divide(row['Value 1'], row['Value 2']), float(row['Result']))
+        print("Completed Division Test")
 
     # def test_division_method_calculator_by_zero(self):
     #     # self.assertEqual(self.calculator.divide((1234, 0), "Cannot divide by 0!"),
     #     with self.assertRaises(ZeroDivisionError):
     #         self.calculator.divide(12, 0)
 
-    # def test_square_method_calculator(self):
-    #     test_data = CsvReader('/src/Unit Test Square.csv').data
-    #     pprint(test_data)
-    #     for row in test_data:
-    #         self.assertEqual(self.calculator.square(row['Value 1']), int(row['Result']))
-    #         self.assertEqual(self.calculator.result, int(row['Result']))
+    def test_square_method_calculator(self):
+        print("Begin Square Test")
+        test_data = CsvReader('/src/Unit Test Square.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.square(row['Value 1']), int(row['Result']))
+        print("Completed Square Test")
 
-    # def test_sqrt_method_calculator(self):
-    #     test_data = CsvReader('/src/Unit Test Square Root.csv').data
-    #     pprint(test_data)
-    #     for row in test_data:
-    #         self.assertAlmostEqual(self.calculator.sqrt(float(row['Value 1'])), float(row['Result']))
-    #         self.assertAlmostEqual(self.calculator.result, float(row['Result']))
+    def test_sqrt_method_calculator(self):
+        print("Begin Square Root Test")
+        test_data = CsvReader('/src/Unit Test Square Root.csv').data
+        for row in test_data:
+            self.assertAlmostEqual(self.calculator.sqrt(float(row['Value 1'])), float(row['Result']))
+            self.assertAlmostEqual(self.calculator.result, float(row['Result']))
+        print("Completed Square Root Test")
 
     # def test_sqrt_negatives_method_calculator(self):
     #     with self.assertRaises(ValueError):
