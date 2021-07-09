7f7f3228520a490d77d3ac3bcd8b876d1d0f1a20
ats43 <85419691+ats43@users.noreply.github.com>
2021-07-09 09:54:09 -0400

    feature: added absolutepaths for travis

--- src/CsvReader/csvReader.py
+++ src/CsvReader/csvReader.py
@@ -1,5 +1,6 @@
 import csv
 from pprint import pprint
+from pathlib import Path
 
 
 def class_factory(class_name, dictionary):
@@ -10,7 +11,9 @@ class CsvReader:
 
     def __init__(self, filepath):
         self.data = []
-        with open(filepath) as text_data:
+        relative = Path(filepath)
+        absolute = relative.absolute()
+        with open(absolute) as text_data:
             csv_data = csv.DictReader(text_data, delimiter=',')
             for row in csv_data:
                 self.data.append(row)
