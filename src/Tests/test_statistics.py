694cfa54916634d24b6cf3479462640f8ec58a2f
ats43 <85419691+ats43@users.noreply.github.com>
2021-07-11 09:53:00 -0400

    feature: added stats test setup and instantiate stats

--- src/Tests/test_statistics.py
+++ src/Tests/test_statistics.py
@@ -1,9 +1,18 @@
 import unittest
+from src.CsvReader.csvReader import CsvReader
+from src.Calculator.Calculator import Calculator
+from src.Statistics.Statistics import Statistics
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)
+
+    def setUp(self) -> None:
+        self.statistics = Statistics()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.statistics, Statistics)
 
 
 if __name__ == '__main__':
